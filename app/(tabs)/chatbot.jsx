79956fbba1d2e88ef35d1579b88d3dfb23d14cda
Tanish Dhariwal <tanishdhariwal21@gmail.com>
2024-11-07 16:10:37 +0530

    Chatbot tab init

--- app/(tabs)/chatbot.jsx
+++ app/(tabs)/chatbot.jsx
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function Discover() {
+export default function ChatBot() {
   return (
     <View>
-      <Text>Discover</Text>
+      <Text>ChatBot</Text>
     </View>
   )
 }
