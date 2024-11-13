b05cf36192c1198f508929b3a30be37b9ea2e9a2
WaryFriend456 <kushalbang456@gmail.com>
2024-11-14 01:43:51 +0530

    Working app

--- app/auth/Sign-in/index.js
+++ app/auth/Sign-in/index.js
@@ -25,7 +25,7 @@ const onSignIn=()=>{
         ToastAndroid.show("Please enter all details",ToastAndroid.CENTER)
     }
 signInWithEmailAndPassword(auth, email, password)
-router.replace('/myTrip')
+router.replace('/chatbot')
   .then((userCredential) => {
     // Signed in 
     const user = userCredential.user;
