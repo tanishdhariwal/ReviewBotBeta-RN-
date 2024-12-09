986e48cadc2ebcf69479b2f0c5345ef02eacce98
Tanish Dhariwal <tanishdhariwal21@gmail.com>
2024-12-09 13:15:01 +0530

    changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,6 @@
 import { Stack } from "expo-router";
 import {useFonts} from "expo-font";
+
 export default function RootLayout() {
 
   useFonts({
--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,8 @@
 import { Text, View } from "react-native";
 import Login from '../components/Login'
+
 import {auth} from './../configs/Firebase_Config'
+
 export default function Index() {
 
   const user = auth.currentUser;
